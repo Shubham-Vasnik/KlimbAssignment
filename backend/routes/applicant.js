ebbac8f4790a418dab829bebb25bd7a134650a2a
Shubham Vasnik <shubhamvasnik20@gmail.com>
2021-01-11 19:30:28 +0530

    added frontend routes

--- backend/routes/applicant.js
+++ backend/routes/applicant.js
@@ -1,64 +1,14 @@
 const express = require('express');
 
-const Applicant = require('../models/Applicant');
-const xlToJson = require('../helperFunctions/xlToJson');
+const {documentUpload} = require('../middleware/fileHelper');
+const { findApplicants, addApplicants } = require('../controllers/applicant');
 
 const router = express.Router();
 
 
-router.get('/', (req, res, next) => {
+router.get('/', findApplicants);
 
-    Applicant.find().then(
-        result => {
-            res.status(200).json({
-                message:"Found Applicants",
-                applicants:result,
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                message:"No Applicant Found",
-                error:err
-            });
-        })
-});
-
-router.post('/', (req, res, next) => {
-
-    let response = xlToJson('./public/uploads/test.xlsx','Sheet1');
-
-    let data = response[1];
-    console.log(data);
-
-    let applicant = {
-        name:data['Name of the Candidate'],
-        email:data['Email'],
-        mobileNo:data['Mobile No.'],
-        dateOfBirth:data['Date of Birth'],
-        workExperience:data['Work Experience'],
-        resumeTitle:data['Resume Title'],
-        currentLocation:data['Current Location'],
-        postalAddress:data['Postal Address'],
-        currentEmployer:data['Current Employer'],
-        currentDesignation:data['Current Designation']
-    }
-
-    console.log(applicant);
-
-    Applicant.create(applicant).then(
-        result => {
-            res.status(200).json({
-                message:"Applicants Added",
-                applicants:result
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                message:"Unable to Add Applicants",
-                error:err
-            });
-        })
-});
+router.post('/', documentUpload.single('myFile'), addApplicants );
 
 
 
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import AddApplicants from './AddApplicants';
+import ApplicantList from './ApplicantList';
+
+import Navbar from './Navbar';
 
 const App = () => {
     return (
         <div>
-            Klimb
+            <Router>
+                <Navbar/>
+                <Switch>
+                    <Route exact path='/' component={AddApplicants} />
+                    <Route exact path='/applicants' component={ApplicantList} />
+                </Switch>
+            </Router>
         </div>
     );
 }
